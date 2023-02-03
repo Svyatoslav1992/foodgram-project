2c640c068680b89152ce864fbc0319356242ded5
Слава Журавлев <Slavatron92@gmail.com>
2023-02-03 16:37:48 +0700

    Деплой

--- backend/recipes/management/commands/load_users.py
+++ backend/recipes/management/commands/load_users.py
@@ -8,9 +8,9 @@
 
 #     def handle(self, *args, **kwargs):
 #         data = [
-#             {'username': 'Ivan', 'first_name': 'Ivan', 'last_name': 'Ivan',
+#             {'username': 'Ivan', 'first_name': 'Иван', 'last_name': 'Дор',
 #                 'email': 'Ivan@gmail.com', 'password': '1q2w3e4r'},
-#             {'username': 'Slava', 'first_name': 'Слава', 'last_name': 'Килин',
+#             {'username': 'Slava', 'first_name': 'Слава', 'last_name': 'Киин',
 #                 'email': 'Svyatoslav@gmail.com', 'password': '1q2w3e4r'},
 #             {'username': 'Darya', 'first_name': 'Дарья', 'last_name': 'Ан',
 #                 'email': 'Darya@gmail.com', 'password': '1q2w3e4r'},
