3a56bf9ee919bae0a1bd277e1d8e60ce05a30f95
Слава Журавлев <Slavatron92@gmail.com>
2023-02-03 14:52:35 +0700

    Деплой

--- backend/recipes/management/commands/load_recipes.py
+++ backend/recipes/management/commands/load_recipes.py
@@ -1,14 +1,18 @@
 from django.core.management import BaseCommand
 
 from recipes.models import Recipe
+from users.models import User
+
 
 
 class Command(BaseCommand):
     help = 'Создаем рецепты'
 
     def handle(self, *args, **kwargs):
+        ivan = User.objects.get(username="Ivan")
+        print(ivan)
         data = [
-            {'name': 'Суп', 'text': 'Тот самый суп', 'image': 'https://avatars.mds.yandex.net/i?id=7ab3f5b759042e7890c08012ba6ab4a5-4032833-images-thumbs&n=13', 'cooking_time': '7', 'tags': 'завтрак', 'author': "Ivan", 'ingredients': '[{ "id": 1123, "amount": 10}]'},
+            {'name': 'Суп', 'text': 'Тот самый суп', 'image': 'https://avatars.mds.yandex.net/i?id=7ab3f5b759042e7890c08012ba6ab4a5-4032833-images-thumbs&n=13', 'cooking_time': '7', 'tags': 'завтрак', 'author': Ivan, 'ingredients': '[{ "id": 1123, "amount": 10}]'},
         ]
         Recipe.objects.bulk_create(Recipe(**recipes) for recipes in data)
         self.stdout.write(self.style.SUCCESS('Все рецепты созданы!'))
