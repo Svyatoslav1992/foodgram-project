4445e52e7b8193e6252d2dda0b6ed3ce376eb14d
Слава Журавлев <Slavatron92@gmail.com>
2023-02-03 13:39:47 +0700

    Деплой

--- backend/recipes/management/commands/load_recipes.py
+++ backend/recipes/management/commands/load_recipes.py
@@ -1,15 +1,15 @@
-# from django.core.management import BaseCommand
+from django.core.management import BaseCommand
 
-# from recipes.models import Recipe
+from recipes.models import Recipe
 
 
-# class Command(BaseCommand):
-#     help = 'Создаем рецепты'
+class Command(BaseCommand):
+    help = 'Создаем рецепты'
 
-#     def handle(self, *args, **kwargs):
-#         data = [
-#             {'name': 'Суп', 'text': 'Из семи залуп', 'image': 'https://avatars.mds.yandex.net/i?id=7ab3f5b759042e7890c08012ba6ab4a5-4032833-images-thumbs&n=13', 'cooking_time': '7', 'tags': 'завтрак', 'author': '', 'ingredients': ''},
-#         ]
-#         Recipe.objects.bulk_create(Recipe(**recipes) for recipes in data)
-#         self.stdout.write(self.style.SUCCESS('Все рецепты созданы!'))
+    def handle(self, *args, **kwargs):
+        data = [
+            {'name': 'Суп', 'text': 'Из семи залуп', 'image': 'https://avatars.mds.yandex.net/i?id=7ab3f5b759042e7890c08012ba6ab4a5-4032833-images-thumbs&n=13', 'cooking_time': '7', 'tags': 'завтрак', 'author': 'Ivan', 'ingredients': '[{ "id": 1123, "amount": 10}]'},
+        ]
+        Recipe.objects.bulk_create(Recipe(**recipes) for recipes in data)
+        self.stdout.write(self.style.SUCCESS('Все рецепты созданы!'))
 
--- backend/recipes/management/commands/load_users.py
+++ backend/recipes/management/commands/load_users.py
@@ -8,9 +8,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data = [
-            {'username': 'Ivan', 'first_name': 'Иван', 'last_name': 'Фёдоров', 'email': 'Ivan@gmail.com', 'password': '1q2w3e4r'},
-            {'username': 'Slava', 'first_name': 'Святосдав', 'last_name': 'Килин', 'email': 'Svyatoslav@gmail.com', 'password': '1q2w3e4r'},
-            {'username': 'Darya', 'first_name': 'Дарья', 'last_name': 'Долгоппупс', 'email': 'Darya@gmail.com', 'password': '1q2w3e4r'},
+            {'username': 'Ivan', 'first_name': 'Иван', 'last_name': 'Фёдоров',
+                'email': 'Ivan@gmail.com', 'password': '1q2w3e4r'},
+            {'username': 'Slava', 'first_name': 'Слава', 'last_name': 'Килин',
+                'email': 'Svyatoslav@gmail.com', 'password': '1q2w3e4r'},
+            {'username': 'Darya', 'first_name': 'Дарья', 'last_name': 'Ан',
+                'email': 'Darya@gmail.com', 'password': '1q2w3e4r'},
         ]
         User.objects.bulk_create(User(**user) for user in data)
         self.stdout.write(self.style.SUCCESS('Все пользователи созданы!'))
