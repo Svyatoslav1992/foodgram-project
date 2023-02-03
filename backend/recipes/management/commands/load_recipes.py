4c1f79beb0d07a215fb699c80a90a508cee3cb5b
Слава Журавлев <Slavatron92@gmail.com>
2023-02-03 14:21:52 +0700

    Деплой

--- backend/recipes/management/commands/load_recipes.py
+++ backend/recipes/management/commands/load_recipes.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data = [
-            {'name': 'Суп', 'text': 'Тот самый суп', 'image': 'https://avatars.mds.yandex.net/i?id=7ab3f5b759042e7890c08012ba6ab4a5-4032833-images-thumbs&n=13', 'cooking_time': '7', 'tags': 'завтрак', 'author': Ivan, 'ingredients': '[{ "id": 1123, "amount": 10}]'},
+            {'name': 'Суп', 'text': 'Тот самый суп', 'image': 'https://avatars.mds.yandex.net/i?id=7ab3f5b759042e7890c08012ba6ab4a5-4032833-images-thumbs&n=13', 'cooking_time': '7', 'tags': 'завтрак', 'author': '', 'ingredients': '[{ "id": 1123, "amount": 10}]'},
         ]
         Recipe.objects.bulk_create(Recipe(**recipes) for recipes in data)
         self.stdout.write(self.style.SUCCESS('Все рецепты созданы!'))
