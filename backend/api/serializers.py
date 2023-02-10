78191f9023e93356f546555bf5fb265882b9cf77
Слава Журавлев <Slavatron92@gmail.com>
2023-02-10 08:04:55 +0700

    Деплой

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 
 from recipes.models import IngredientRecipe, Ingredient, Recipe, Tag
 from users.models import User
-from .fields import Base64ImageField
+
+import base64
+
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
 
 
 class UserListSerializer(serializers.ModelSerializer):
