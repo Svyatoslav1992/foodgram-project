1ea00196872541d49134d1a93a6347f05c0165db
Слава Журавлев <Slavatron92@gmail.com>
2023-02-21 14:40:15 +0700

    Деплой

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -35,60 +35,99 @@ def delete_from(self, model, user, pk):
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-def download_cart(cart):
-    """ Выгрузка списка покупок в pdf """
+# def download_cart(cart):
+#     """ Выгрузка списка покупок в pdf """
 
-    buffer = BytesIO()
-    can = canvas.Canvas(buffer)
+#     buffer = BytesIO()
+#     can = canvas.Canvas(buffer)
 
+#     SANS_REGULAR = settings.STATIC_ROOT + '/fonts/OpenSans-Regular.ttf'
+#     SANS_REGULAR_NAME = 'OpenSans-Regular'
+#     SANS_BOLD = settings.STATIC_ROOT + '/fonts/OpenSans-Bold.ttf'
+#     SANS_BOLD_NAME = 'OpenSans-Bold'
+
+#     FILE_NAME = 'shopping_list.pdf'
+
+#     pdfmetrics.registerFont(TTFont(SANS_REGULAR_NAME, SANS_REGULAR))
+#     pdfmetrics.registerFont(TTFont(SANS_BOLD_NAME, SANS_BOLD))
+#     can.setTitle('Список покупок')
+
+#     can.setFont(SANS_BOLD_NAME, 32)
+#     can.drawString(100, 750, 'foodgram')
+#     can.setFont(SANS_REGULAR_NAME, 13)
+#     can.drawString(100, 725, 'Ваш продуктовый помощник')
+#     can.line(100, 715, 500, 715)
+#     can.setFont(SANS_BOLD_NAME, 13)
+#     can.drawString(393, 700, 'cписок покупок')
+#     can.setFont(SANS_REGULAR_NAME, 10)
+
+#     padding_top = 680
+#     page_number = 1
+#     steps = len(cart)
+#     last_step = steps - 1
+#     for step, ingredient in enumerate(cart):
+#         ingredient = list(ingredient.values())
+#         if step == last_step and steps >= 3:
+#             x = padding_top - 20
+#             can.line(100, x, 500, x)
+#             can.setFont(SANS_BOLD_NAME, 13)
+#             can.drawString(362, x - 15, 'приятного аппетита')
+#             can.setFont(SANS_REGULAR_NAME, 10)
+#         if padding_top <= 80:
+#             can.showPage()
+#             can.line(100, 770, 500, 770)
+#             can.setFont(SANS_BOLD_NAME, 13)
+#             can.drawString(393, 755, 'cписок покупок')
+#             can.setFont(SANS_REGULAR_NAME, 10)
+#             page_number += 1
+#             padding_top = 735
+#             can.drawString(100, 780, f'стр. {page_number}')
+#         product = ingredient[0]
+#         unit = ingredient[1]
+#         total = ingredient[2]
+#         string = f'{step+1}. {product[0:48]} ({unit[0:10]}) — {total}'
+#         can.drawString(100, padding_top, string)
+#         padding_top -= 19
+
+#     can.save()
+#     buffer.seek(0)
+#     return FileResponse(buffer, as_attachment=True, filename=FILE_NAME)
+
+def download_cart(list_ing):
     SANS_REGULAR = settings.STATIC_ROOT + '/fonts/OpenSans-Regular.ttf'
     SANS_REGULAR_NAME = 'OpenSans-Regular'
     SANS_BOLD = settings.STATIC_ROOT + '/fonts/OpenSans-Bold.ttf'
     SANS_BOLD_NAME = 'OpenSans-Bold'
 
-    FILE_NAME = 'shopping_list.pdf'
-
     pdfmetrics.registerFont(TTFont(SANS_REGULAR_NAME, SANS_REGULAR))
     pdfmetrics.registerFont(TTFont(SANS_BOLD_NAME, SANS_BOLD))
-    can.setTitle('Список покупок')
 
-    can.setFont(SANS_BOLD_NAME, 32)
-    can.drawString(100, 750, 'foodgram')
-    can.setFont(SANS_REGULAR_NAME, 13)
-    can.drawString(100, 725, 'Ваш продуктовый помощник')
-    can.line(100, 715, 500, 715)
-    can.setFont(SANS_BOLD_NAME, 13)
-    can.drawString(393, 700, 'cписок покупок')
-    can.setFont(SANS_REGULAR_NAME, 10)
+    buffer = io.BytesIO()
+    c = canvas.Canvas(buffer)
 
-    padding_top = 680
-    page_number = 1
-    steps = len(cart)
-    last_step = steps - 1
-    for step, ingredient in enumerate(cart):
-        ingredient = list(ingredient.values())
-        if step == last_step and steps >= 3:
-            x = padding_top - 20
-            can.line(100, x, 500, x)
-            can.setFont(SANS_BOLD_NAME, 13)
-            can.drawString(362, x - 15, 'приятного аппетита')
-            can.setFont(SANS_REGULAR_NAME, 10)
-        if padding_top <= 80:
-            can.showPage()
-            can.line(100, 770, 500, 770)
-            can.setFont(SANS_BOLD_NAME, 13)
-            can.drawString(393, 755, 'cписок покупок')
-            can.setFont(SANS_REGULAR_NAME, 10)
-            page_number += 1
-            padding_top = 735
-            can.drawString(100, 780, f'стр. {page_number}')
+    c.setFont(SANS_BOLD_NAME, 32)
+    c.drawString(30, 775, 'Foodgram')
+
+    c.setFont(SANS_REGULAR_NAME, 20)
+    c.drawString(30, 740, 'Ваш продуктовый помошник')
+    c.line(30, 730, 580, 730)
+
+    c.drawString(30, 710, 'Список покупок')
+    val = 680
+    for step, ing in enumerate(list_ing):
+        ingredient = list(ing.values())
         product = ingredient[0]
         unit = ingredient[1]
-        total = ingredient[2]
-        string = f'{step+1}. {product[0:48]} ({unit[0:10]}) — {total}'
-        can.drawString(100, padding_top, string)
-        padding_top -= 19
+        amount = ingredient[2]
+        string = f'{step+1}. {product} {unit} - {amount}'
+        c.drawString(30, val, string)
+        val -= 20
 
-    can.save()
+    c.showPage()
+    c.save()
     buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True, filename=FILE_NAME)
+    return FileResponse(
+        buffer,
+        as_attachment=True,
+        filename='shoppcart_list.pdf'
+    )
--- backend/api/views.py
+++ backend/api/views.py
@@ -11,8 +11,8 @@ from api.filters import IngredientFilter, RecipeFilter
 from api.pagination import CustomPagination
 from api.serializers import (IngredientSerializer, RecipeReadSerializer,
                              RecipeWriteSerializer, TagSerializer, RecipeShortInfo)
-# from api.utils import add_to, delete_from, download_cart
-from api.utils import add_to, delete_from
+from api.utils import add_to, delete_from, download_cart
+# from api.utils import add_to, delete_from
 
 
 
@@ -111,7 +111,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 class DownloadCart(APIView):
     """Вью для скачивания списка покупок."""
-
+    permission_classes = [permissions.IsAuthenticated]
     def get(self, request):
         # list_ing = request.user.recipe_shopping_cart.values(
         list_ing = request.user.user_shopping_cart.values(
@@ -130,40 +130,40 @@ class DownloadCart(APIView):
 #         return self.delete_obj(ShoppingCart, request.user, pk)
 #     return None
 
-@action(detail=False, methods=['get'],
-        permission_classes=[permissions.IsAuthenticated])
-def download_cart(self, request):
-    final_list = {}
-    ingredients = IngredientRecipe.objects.filter(
-        recipe__cart__user=request.user).values_list(
-        'ingredient__name', 'ingredient__measurement_unit',
-        'amount')
-    for item in ingredients:
-        name = item[0]
-        if name not in final_list:
-            final_list[name] = {
-                'measurement_unit': item[1],
-                'amount': item[2]
-            }
-        else:
-            final_list[name]['amount'] += item[2]
-    pdfmetrics.registerFont(
-        TTFont('Slimamif', 'Slimamif.ttf', 'UTF-8'))
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = ('attachment; '
-                                        'filename="shopping_list.pdf"')
-    page = canvas.Canvas(response)
-    page.setFont('Slimamif', size=24)
-    page.drawString(200, 800, 'Список ингредиентов')
-    page.setFont('Slimamif', size=16)
-    height = 750
-    for i, (name, data) in enumerate(final_list.items(), 1):
-        page.drawString(75, height, (f'<{i}> {name} - {data["amount"]}, '
-                                        f'{data["measurement_unit"]}'))
-        height -= 25
-    page.showPage()
-    page.save()
-    return response
+# @action(detail=False, methods=['get'],
+#         permission_classes=[permissions.IsAuthenticated])
+# def download_cart(self, request):
+#     final_list = {}
+#     ingredients = IngredientRecipe.objects.filter(
+#         recipe__cart__user=request.user).values_list(
+#         'ingredient__name', 'ingredient__measurement_unit',
+#         'amount')
+#     for item in ingredients:
+#         name = item[0]
+#         if name not in final_list:
+#             final_list[name] = {
+#                 'measurement_unit': item[1],
+#                 'amount': item[2]
+#             }
+#         else:
+#             final_list[name]['amount'] += item[2]
+#     pdfmetrics.registerFont(
+#         TTFont('Slimamif', 'Slimamif.ttf', 'UTF-8'))
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = ('attachment; '
+#                                         'filename="shopping_list.pdf"')
+#     page = canvas.Canvas(response)
+#     page.setFont('Slimamif', size=24)
+#     page.drawString(200, 800, 'Список ингредиентов')
+#     page.setFont('Slimamif', size=16)
+#     height = 750
+#     for i, (name, data) in enumerate(final_list.items(), 1):
+#         page.drawString(75, height, (f'<{i}> {name} - {data["amount"]}, '
+#                                         f'{data["measurement_unit"]}'))
+#         height -= 25
+#     page.showPage()
+#     page.save()
+#     return response
 
 # def add_obj(self, model, user, pk):
 #     if model.objects.filter(user=user, recipe__id=pk).exists():
