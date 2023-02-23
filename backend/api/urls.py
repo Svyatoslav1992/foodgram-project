c8373fa7736029b9758b67ce1ae491bcb7fa744d
Слава Журавлев <Slavatron92@gmail.com>
2023-02-23 10:19:31 +0700

    Деплой

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,10 @@ from django.contrib.auth import get_user_model
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import (DownloadCart, IngredientViewSet, RecipeViewSet,
-                       TagViewSet)
+# from api.views import (DownloadCart, IngredientViewSet, RecipeViewSet,
+#                        TagViewSet)
+
+from api.views import (IngredientViewSet, RecipeViewSet, TagViewSet)
 
 
 from users.views import UsersViewSet
